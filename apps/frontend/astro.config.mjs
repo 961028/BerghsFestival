cff73be0572caf778c7a3b232e49ea0b4fb9ed2a
Stefan Fisk <contact@stefanfisk.com>
2026-03-15 06:48:58 +0100

    Use astro dev

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -8,10 +8,22 @@ import { loadEnv } from "vite";
 const { NODE_ENV } = process.env;
 assert(NODE_ENV);
 
-const { APP_HOST } = loadEnv(NODE_ENV, process.cwd(), "");
+const { APP_HOST, APP_HOME } = loadEnv(NODE_ENV, process.cwd(), "");
+assert(APP_HOST);
+assert(APP_HOME);
 
 // https://astro.build/config
 export default defineConfig({
+    site: APP_HOME,
+    server: ({ command }) => ({
+        allowedHosts: [APP_HOST],
+        host: '127.0.0.1', // Apache needs this
+    }),
+    vite: {
+        server: {
+            strictPort: true,
+        },
+    },
     image: {
         domains: [APP_HOST],
         layout: 'constrained',
